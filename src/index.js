9ceae76e7ea768b814d3abe6b7ba1e4693fdd23c
Filip Janevski <filip.janevski@yandex.com>
2020-03-24 19:09:56 +0100

    Add jsconfig.json configuration for absolute imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/main.css";
-import App from "./components/App/App";
-import * as serviceWorker from "./serviceWorker";
+import "styles/main.css";
+import App from "components/App/App";
+import * as serviceWorker from "serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
